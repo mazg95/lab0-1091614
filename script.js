669f372c4726d1315e79a7dfd385e8c88a4cc332
Marco <mazg95@live.com>
2018-09-04 21:53:59 -0600

    se agregan funciones js y clases css

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function start(){
     var nav_bar = $(".nav")
-    var fns =  [say_hi,change_bg, change_color, change_letter, change_font_size, set_to_default] 
+    var fns =  [say_hi,change_bg, change_color, change_letter, change_font_size] 
+    var tags = ['Alert Fun', 'Change BG' ,'Change Color', 'Change Font', 'Change Font Size', ]
     var i = 0;
     console.log(fns);
     fns.forEach(function(fn){
-        i += 1;
         var btn = $("<li></li>").append($("<a></a>", {
-            text: 'Change ' + i,
+            text: tags[i++],
             click: fn
         }));
         nav_bar.append(btn);
@@ -14,67 +14,38 @@ function start(){
 }
 
 function say_hi(){
-    var alert = document.querySelector(".div-alerts > row > alert ")
-    if(alert.)
+    var alert = $(".div-alerts > .row > .alert ");
+    changeClassElement(alert, 'hide');
+    
 }
 
 function change_letter(){
-
+    var container = $('body');
+    changeClassElement(container, 'star-wars');
 }
 
 function change_bg(){
-
+    var container = $('.site-wrapper');
+    changeClassElement(container, 'expanded');
 }
 
 function change_color(){
-
+    var container = $('body');
+    changeClassElement(container, 'gold');
 }
 
 function change_font_size(){
 
 }
 
-function set_to_default(){
-
-}
-/*
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.inner').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+function changeClassElement(el, c){
+    if(el.hasClass(c)){
+        el.removeClass(c);
     }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.inner').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.inner').removeClass('nav-up').addClass('nav-down');
-        }
+    else{
+        el.addClass(c);
     }
-    
-    lastScrollTop = st;
 }
-*/
+
+
 $(document).ready(start);
